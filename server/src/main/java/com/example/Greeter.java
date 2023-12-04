cf7155bac8d0d6858947000e3f2a9ecb56d4c3df
pbdurand <152877863+pbdurand@users.noreply.github.com>
2023-12-04 16:13:45 +0100

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -7,7 +7,10 @@ public class Greeter {
   public Greeter() {
   /* commentaire : fonction de bienvenu */ 
   }
-
+  /**
+  * @param someone name of person
+  * @return greeting string
+  **/
   public final String greet(final String someone) {
     return String.format("Hello babacar, %s!", someone);
   }
