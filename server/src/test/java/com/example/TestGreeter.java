7912cedc7030d8a4c99f54957c98e70fa0310256
pbdurand <152877863+pbdurand@users.noreply.github.com>
2023-12-05 10:49:53 +0100

    Update TestGreeter.java

--- server/src/test/java/com/example/TestGreeter.java
+++ server/src/test/java/com/example/TestGreeter.java
@@ -19,8 +19,8 @@ public class TestGreeter {
 
   @Test
   public void greetShouldIncludeTheOneBeingGreeted() {
-    String someone = "World";
-    String someone1 = "World";
+    private static final String someone = "World";
+    private static final String someone1 = "World";
 
     assertThat(greeter.greet(someone), containsString(someone));
     //assertThat(greeter.greet(someone), is(someone1));
